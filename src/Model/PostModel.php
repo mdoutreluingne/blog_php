a758fb5615caaa579d5b7a64667a9a884fa139bd
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2021-10-02 12:04:32 +0200

    Change filter created_at by updated_at

--- src/Model/PostModel.php
+++ src/Model/PostModel.php
@@ -15,7 +15,7 @@ class PostModel extends MainModel
      */
     public function listLastPosts()
     {
-        $query = "SELECT post.*, user.last_name, user.first_name FROM " . $this->table . " JOIN user ON user.id = post.user_id ORDER BY created_at DESC LIMIT 3";
+        $query = "SELECT post.*, user.last_name, user.first_name FROM " . $this->table . " JOIN user ON user.id = post.user_id ORDER BY updated_at DESC LIMIT 3";
 
         return $this->database->getAllData($query);
     }
@@ -26,7 +26,7 @@ class PostModel extends MainModel
      */
     public function listPosts(int $firstPage, int $perPage)
     {
-        $query = "SELECT post.*, user.last_name, user.first_name, (SELECT COUNT(c.post_id) FROM comment c WHERE c.post_id = post.id AND c.validated = 1) as countComment FROM " . $this->table . " JOIN user ON user.id = post.user_id ORDER BY post.created_at DESC LIMIT " . $firstPage . ", " . $perPage;
+        $query = "SELECT post.*, user.last_name, user.first_name, (SELECT COUNT(c.post_id) FROM comment c WHERE c.post_id = post.id AND c.validated = 1) as countComment FROM " . $this->table . " JOIN user ON user.id = post.user_id ORDER BY post.updated_at DESC LIMIT " . $firstPage . ", " . $perPage;
 
         return $this->database->getAllData($query);
     }
